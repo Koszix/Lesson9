b66675dd791dd714155272c01b0675cb35c4930b
Koszix <koksyak69@gmail.com>
2024-09-11 16:43:04 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,10 +1,10 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 start = 0
-positive = []
 while start < len(my_list):
-    if my_list[start] >= 1:
-        print(my_list[start])
+    if my_list[start] <= 0:
         start += 1
         continue
-    positive.append(my_list[start])
+    print(my_list[start])
     start += 1
+    if my_list[start] < -1:
+        break
